a5084211c33a289bf0ea954dbf241104476b8d95
Reyner8 <reynerneo08@gmail.com>
2023-06-10 13:25:15 +0800

    update dokumentasi

--- app/Controllers/Admin/Dokumentasi.php
+++ app/Controllers/Admin/Dokumentasi.php
@@ -25,23 +25,17 @@ class Dokumentasi extends BaseController
             'judul' => 'Admin | Dokumentasi',
             'validation' => \Config\Services::validation(),
             'listDokumentasi' => $this->RelationTable->getDokumentasiByIdKejadian($idKejadian),
-            'isEdit' => false
+            'kejadian' => $this->KejadianModel->find($idKejadian),
         ]);
     }
 
     // route : admin/petugas/save
     public function save($idKejadian)
     {
+//         $files = $this->request->getFileMultiple('files');
+// dd($files);
         $rules = [
-            'files' => [
-                'label' => 'Image File',
-                'rules' => [
-                    'uploaded[files]',
-                    'is_image[files]',
-                    'mime_in[files,image/jpg,image/jpeg,image/gif,image/png,image/webp]',
-                    'max_size[files,100]',
-                ],
-            ],
+            'files' => ['uploaded[files]'],
         ];
 
         if (!$this->validate($rules)) {
@@ -49,14 +43,12 @@ class Dokumentasi extends BaseController
             return redirect()->to('admin/kejadian/dokumentasi/'. $idKejadian)->withInput();
         }
 
-        $files = $this->request->getFileMultiple('files');
-
         if ($imagefile = $this->request->getFiles()) {
             foreach ($imagefile['files'] as $img) {
                 if ($img->isValid() && !$img->hasMoved()) {
                     $newName = $img->getRandomName();
-                    $img->move('/assets/uploads/galeri', $newName);
-
+                    // dd($newName);
+                    $img->move('assets/uploads/galeri/', $newName);
                     $this->DokumentasiModel->save([
                         'idKejadian' => $idKejadian,
                         'file' => $newName
@@ -69,15 +61,18 @@ class Dokumentasi extends BaseController
         }
 
         session()->setFlashdata('msg-success', 'Data berhasil diupload!!!');
-        return redirect()->to('admin/kejadian/penugasan/'. $idKejadian);
+        return redirect()->to('admin/kejadian/dokumentasi/'. $idKejadian);
     }
 
-    // route : admin/kejadian/(:num)
+    // route : admin/kejadian/dokumentasi/(:num)
     public function delete($id)
     {
-        $idKejadian = $this->PenugasanModel->find($id)['idKejadian'];
-        $this->PenugasanModel->where('id', $id)->delete();
+        $dokumentasi = $this->DokumentasiModel->find($id);
+        $idKejadian = $dokumentasi['idKejadian'];
+        $file = $dokumentasi['file'];
+        $this->DokumentasiModel->delete($id);
+        unlink('assets/uploads/galeri/'. $file);
         session()->setFlashdata('msg-delete', 'Data berhasil dihapus!!!');
-        return redirect()->to('admin/kejadian/penugasan/'. $idKejadian);
+        return redirect()->to('admin/kejadian/dokumentasi/'. $idKejadian);
     }
 }
